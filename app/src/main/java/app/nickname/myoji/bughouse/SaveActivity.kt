50240bfe4573596318e8cd678dbaef063cadedcc
omae <pasta@omae.lit>
2020-07-17 20:15:58 +0900

    目立ったエラーの修正 Buildはできるようになった

--- app/src/main/java/app/nickname/myoji/bughouse/SaveActivity.kt
+++ app/src/main/java/app/nickname/myoji/bughouse/SaveActivity.kt
@@ -9,12 +9,14 @@ class SaveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load)
+
+
+        saveButton.setOnClickListener {
+            val sharedPreferences = getSharedPreferences("Second", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            val text = input.text.toString()
+            editor.putString("SAVE", text)
+        }
     }
 
-    val sharedPreferences = getSharedPreferences("Second", Context.MODE_PRIVATE)
-    val editor = sharedPreferences.edit()
-    saveButton.setOnClickListener {
-        val text = input.text
-        editor.putString("SAVE", text)
-    }
 }
