2272e01b6bf8f4502d237b38d56817ee0517d633
omae <pasta@omae.lit>
2020-07-18 21:19:21 +0900

    Imageのバグを(たぶん)直した

--- app/src/main/java/app/nickname/myoji/bughouse/ImageViewActivity.kt
+++ app/src/main/java/app/nickname/myoji/bughouse/ImageViewActivity.kt
@@ -3,23 +3,23 @@ package app.nickname.myoji.bughouse
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import coil.api.load
 import kotlinx.android.synthetic.main.activity_image_view.*
 
 class ImageViewActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_view)
 
         imageView.load("https://life-is-tech.com/materials/images/summer2019_desktop_3.jpg")
 
         getImageButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+            val intent = Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
             intent.type = "image/*"
-            startActivityForResult(intent, 100)
+            startActivityForResult(intent, 1)
         }
 
     }
@@ -27,7 +27,7 @@ class ImageViewActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode == 1 && resultCode == Activity.RESULT_CANCELED) {
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
             imageView.load(data?.data)
         }
     }
